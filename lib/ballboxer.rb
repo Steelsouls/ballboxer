0133b6568e66815609ef8c51d6c2016900dec35e
Brandon Rubin <Steelsouls@gmail.com>
2013-11-17 18:04:29 -0800

    Add collision with ball, defeat animation, lives, immunity after death, immunity while swinging.

--- lib/ballboxer.rb
+++ lib/ballboxer.rb
@@ -5,38 +5,88 @@
 require 'gosu'
 
 module ZOrder
-  Background, Player, Ball, UI = 0, 1, 2, 3
+  Background, Player, Ball, Effects, UI = 0, 1, 2, 3, 4
 end
 
-module Animation
-  class SmokeRing
-    def initialize(window)
-      @Gosu::Image::load_tiles(window, "media/smoke_ring_anim.png", 30, 30, false)
-    end
-end
 
 module BallBoxer
-  VERSION = '0.2'
+  VERSION = '0.3'
+
+  # class Animations
+  #   def initialize(window)
+  #     @smoke_ring = Gosu::Image::load_tiles(window, "media/smoke_ring_anim.png", 30, 30, false)
+  #   end
+  # end
 
   class Player
-    attr_accessor :idle
+    attr_accessor :x, :y
+    attr_reader :lives
     def initialize(window)
       @image = Gosu::Image.new(window, "media/dwarf_miner_idle.png", false)
       @swing_image = Gosu::Image.new(window, "media/dwarf_miner_swing.png", false)
-      @defeat_animation = Animation::SmokeRing.new(window)
-      @x = @y = @vel_x = @vel_y = @swing_timer = 0.0
+      @defeat_animation = Gosu::Image::load_tiles(window, "media/smoke_ring_anim_long.png", 30, 30, false)
+      @x = @y = @vel_x = @vel_y = @swing_timer = @defeat_timer = @immunity_timer = @score = 0.0
       @direction = 1
-      @idle = true
-      @score = 0
       @lives = 3
+      @swinging = false
+      @defeated = false
+      @immune = false
+    end
+
+    def width
+      @image.width
+    end
+
+    def height
+      @image.height
+    end
+
+    def interact
+      if @swinging && @swing_timer > 0
+
+      else
+        get_hit unless @immune
+      end
     end
 
     def swing
-      if @idle
-        @swing_timer = 30
-        @idle = false
-      else
+      if @swinging
         @swing_timer -= 1
+      else
+        @swing_timer = 30
+        @swinging = true
+      end
+    end
+
+    def reset_swing
+      @swinging = false
+    end
+
+    def get_hit
+      unless @defeated
+        @lives -= 1
+        @defeated = true
+        @defeat_timer = 30
+      end
+    end
+
+    def respawn
+      @defeat_timer -= 1
+      if @defeat_timer <= 0
+        @x, @y = rand(720), rand(480)
+        @defeated = false
+        @immune = true
+        @immunity_timer = 120
+      end
+    end
+
+    def check_immunity
+      @immunity_timer -= 1
+      @immune = false if @immunity_timer <= 0
+      if @immune
+        @color = Gosu::Color.rgba(255, 140, 17, 255)
+      else
+        @color = Gosu::Color.rgba(255, 255, 255, 255)
       end
     end
 
@@ -61,39 +111,53 @@ module BallBoxer
     end
 
     def move_up
-      @vel_y -= 0.8
+      @vel_y -= 0.9
     end
 
     def move_down
-      @vel_y += 0.8
+      @vel_y += 0.9
     end
 
     def move
-      @x += @vel_x
-      @y += @vel_y
+      @x += @vel_x unless @defeated
+      @y += @vel_y unless @defeated
       @x %= 720
       @y %= 480
 
       @vel_x *= 0.75
       @vel_y *= 0.75
+
+      respawn if @defeated
+      check_immunity
     end
 
     def draw
-      if @idle
-        @image.draw(@x - @image.width/2, @y - @image.height/2, ZOrder::Player, @direction)
+      if @swinging && @swing_timer > 0
+        @swing_image.draw(@x - @image.width/2, @y - @image.height/2, ZOrder::Player, @direction, 1, @color)
       elsif @defeated
-
+        img = @defeat_animation[Gosu::milliseconds / 125 % @defeat_animation.size]
+        img.draw(@x - img.width/2, @y - img.height/2, ZOrder::Effects, @direction, 1, @color)
       else
-        @swing_image.draw(@x - @swing_image.width/2, @y - @swing_image.height/2, ZOrder::Player, @direction)
+        @image.draw(@x - @swing_image.width/2, @y - @swing_image.height/2,
+          ZOrder::Player, @direction, 1, @color)
       end
     end
   end
 
   class Ball
+    attr_accessor :x, :y
     def initialize(window)
       @image = Gosu::Image.new(window, "media/red_ball.png", false)
       @x = @y = @vel_x = @vel_y = 0.0
       @angle = rand(360)
+    end
+
+    def width
+      @image.width
+    end
+
+    def height
+      @image.height
     end
 
     def goto(x, y)
@@ -141,6 +205,8 @@ module BallBoxer
 
       @player = Player.new(self)
       @player.goto(360, 240)
+
+      # @animations = Animations.new(self)
     end
 
     def needs_cursor?
@@ -166,6 +232,10 @@ module BallBoxer
 
       @player.move
       @ball.move
+      if Gosu::distance(@player.x, @player.y, @ball.x + @ball.width/2, @ball.y) < 30
+        @player.interact
+      end
+      close if @player.lives < 0
     end
 
     def draw
@@ -176,7 +246,7 @@ module BallBoxer
 
     def button_up(id)
       if id == Gosu::KbSpace
-        @player.idle = true
+        @player.reset_swing
       end
     end
 
